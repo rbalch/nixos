e5dbdac3f923c80e6ac6b8f9e6b383fddae58ae2
Ryan Balch <ryan@balch.io>
2024-01-03 21:44:43 -0500

    updating nix; first version with gpu docker support for rootless

--- machines/docker.nix
+++ machines/docker.nix
@@ -1,38 +1,21 @@
 { config, pkgs, ... }:
 
 {
-	# systemd.enableUnifiedCgroupHierarchy = false;
-
 	virtualisation.docker = {
 		enable = true;
 		enableOnBoot = false;
 		enableNvidia = true;
-		# extraOptions = "--add-runtime nvidia=/run/current-system/sw/bin/nvidia-container-runtime";
 
 		rootless = {
 			enable = true;
-			setSocketVariable = true;
-            daemon.settings = {
-				# group = "docker";
-				# hosts = [ "fd://" ];
+			setSocketVariable = false;
+			daemon.settings = {
 				runtimes = {
 					nvidia = {
-            			path = "${pkgs.nvidia-docker}/bin/nvidia-container-runtime";
-          			};
+						path = "${pkgs.nvidia-docker}/bin/nvidia-container-runtime";
+					};
 				};
-                # runtimes.nvidia = {
-                #     path = "${pkgs.nvidia-docker}/bin/nvidia-container-runtime";
-                # };
-                # "default-runtime" = "nvidia";
-				# "node-generic-resources" = [
-				# 	"NVIDIA-GPU=da7eed8c-4a16-8163-b0dd-2e77663471a9"
-				# ];
-            };
-			#daemon.settings = ''
-			#	{
-			#		"runtimes": { "custom": { "nvidia": { "path" = "/run/current-system/sw/bin/nvidia-container-runtime" }}}
-			#	}
-			#'';
+			};
 		};
 	};
 	users.extraGroups.docker.members = [ "ryan" ];
