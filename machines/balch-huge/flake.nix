6412103015835a60d091bfce73c4dde265ad98cd
Ryan Balch <ryan@balch.io>
2024-05-06 14:43:17 -0400

    adding gcloud to mac

--- machines/balch-huge/flake.nix
+++ machines/balch-huge/flake.nix
@@ -20,6 +20,7 @@
       # $ nix-env -qaP | grep wget
       environment.systemPackages = with pkgs; [
           fzf
+          google-cloud-sdk
           killall
           ngrok
           python3Full
