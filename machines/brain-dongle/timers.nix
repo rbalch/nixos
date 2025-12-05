4686cf78901d37a998e04c52bbb553a7f3eb76c2
Ryan Balch <ryan@balch.io>
2025-12-05 10:25:51 -0500

    fixing wodify cron

--- machines/brain-dongle/timers.nix
+++ machines/brain-dongle/timers.nix
@@ -1,24 +1,37 @@
 { pkgs, ... }:
 
-{
-    systemd.services.wodify-signup = {
+let
+  dockerBin = "${pkgs.docker}/bin/docker";
+in {
+  systemd.services.wodify-signup = {
     description = "Wodify class signup";
+
     serviceConfig = {
-        User = "ryan";
-        WorkingDirectory = "/home/ryan/code/wodify-signup";
-        ExecStart = ''
-        docker-compose run --rm dev python /app/main.py
-        '';
-        StandardOutput = "append:/var/log/wodify.log";
-        StandardError = "append:/var/log/wodify.log";
-    };
+      User = "ryan";
+      WorkingDirectory = "/home/ryan/code/wodify-signup";
+
+      # Let systemd own the log file in /var/log
+      StandardOutput = "append:/var/log/wodify.log";
+      StandardError  = "append:/var/log/wodify.log";
     };
 
-    systemd.timers.wodify-signup = {
+    # Shell script, like your cron line
+    script = ''
+      ${dockerBin} compose run --rm dev python /app/main.py
+    '';
+  };
+
+  systemd.timers.wodify-signup = {
     wantedBy = [ "timers.target" ];
     timerConfig = {
-        OnCalendar = "Sun-Thu 19:00:00";
-        Persistent = true;
+      OnCalendar = [
+        "Sun *-*-* 19:00:00"
+        "Mon *-*-* 19:00:00"
+        "Tue *-*-* 19:00:00"
+        "Wed *-*-* 19:00:00"
+        "Thu *-*-* 19:00:00"
+      ];
+      Persistent = true;
     };
-    };
+  };
 }
