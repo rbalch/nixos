2b6e663d0f279581b6e13ce997878537eee6684f
Ryan Balch <ryan@balch.io>
2026-02-14 19:01:12 -0500

    patching up docker / nvidia

--- machines/docker.nix
+++ machines/docker.nix
@@ -1,26 +1,16 @@
 { config, pkgs, ... }:
 
 {
-	virtualisation.docker = {
-		enable = true;
-		enableOnBoot = false; # false
+  virtualisation.docker = {
+    enable = true;
+    enableOnBoot = false;
+    rootless = {
+      enable = true;
+      setSocketVariable = false;
+    };
+  };
 
-		# Enable NVIDIA runtime using the deprecated setting as well for compatibility
-		# enableNvidia = true;
+  hardware.nvidia-container-toolkit.enable = true;
 
-		rootless = {
-			enable = true;
-			setSocketVariable = false; # false
-			daemon.settings = {
-				runtimes = {
-					nvidia = {
-						path = "${pkgs.nvidia-docker}/bin/nvidia-container-runtime";
-					};
-				};
-				# Explicitly set the default runtime to use NVIDIA
-				# default-runtime = "nvidia";
-			};
-		};
-	};
-	users.extraGroups.docker.members = [ "ryan" ];
+  users.extraGroups.docker.members = [ "ryan" ];
 }
